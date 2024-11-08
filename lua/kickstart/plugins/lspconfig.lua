4ad24c06aff2aadd8dbf378b63a0a2372b934088
Sébastien Graveline <graveline.seb+dev@gmail.com>
2024-11-07 21:42:37 -0500

    relative number

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -179,6 +179,7 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         --
+        stimulus_ls = {},
 
         lua_ls = {
           -- cmd = {...},
