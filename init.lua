3ac7517286cd120c0a90b4ae0fa1e48235042fb3
Sebastien Graveline <graveline.seb@gmail.com>
2025-04-03 16:04:34 -0400

    Remove markdown + Add hyprls

--- init.lua
+++ init.lua
@@ -690,6 +690,7 @@ require('lazy').setup({
         docker_compose_language_service = {},
         dockerls = {},
         gopls = {},
+        hyprls = {},
         html = {},
         jdtls = {},
         jsonls = {},
@@ -749,6 +750,7 @@ require('lazy').setup({
         'docker_compose_language_service',
         'dockerls',
         'gopls',
+        'hyprls',
         'html',
         'jdtls',
         'jsonls',
@@ -1017,7 +1019,17 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc', --'markdown', --'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -1026,7 +1038,10 @@ require('lazy').setup({
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         disable = { 'latex' },
-        additional_vim_regex_highlighting = { 'latex', 'markdown', 'ruby' },
+        additional_vim_regex_highlighting = {
+          'latex', --'markdown',
+          'ruby',
+        },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
